a14ecf4d29328fbd25a8ce694e0c35e4465a0f73
downson <downson.yuen@gmail.com>
2018-02-19 16:13:51 -0500

    Add greeting with current date and time

--- app/src/main/java/com/csi5175/mobilecommerce/recogu/MainActivity.java
+++ app/src/main/java/com/csi5175/mobilecommerce/recogu/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,13 +22,15 @@ import com.google.android.gms.location.ActivityRecognition;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     public GoogleApiClient mApiClient;
     private String mainName = MainActivity.class.getSimpleName();
     private TextView txtActivity, txtConfidence;
     private ImageView imgActivity;
-    private Button btnStartTracking, btnStopTracking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +40,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         txtActivity = findViewById(R.id.txt_activity);
         txtConfidence = findViewById(R.id.txt_confidence);
         imgActivity = (ImageView) findViewById(R.id.img_activity);
-        btnStartTracking = findViewById(R.id.btn_start_tracking);
-        btnStopTracking = findViewById(R.id.btn_stop_tracking);
+
+        displayGreeting();
 
         mApiClient = new GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
@@ -88,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onResume() {
         super.onResume();
+        displayGreeting();
         startService(new Intent(this, ActivityRecognizedService.class));
     }
 
@@ -95,5 +99,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onPause() {
         super.onPause();
         stopService(new Intent(this, ActivityRecognizedService.class));
+    }
+
+    private void displayGreeting() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = simpleDateFormat.format(calendar.getTime());
+
+        imgActivity.setImageResource(R.drawable.ic_frontpage);
+        txtActivity.setGravity(Gravity.CENTER);
+        txtActivity.setText("Welcome back! \nCurrent date and time is \n" + time);
     }
 }
