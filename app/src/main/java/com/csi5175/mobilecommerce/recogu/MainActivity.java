72c3412f9965d5f450a51e048d9ea10b3568275a
downson <downson.yuen@gmail.com>
2018-02-21 14:47:51 -0500

    Add Google Maps service

--- app/src/main/java/com/csi5175/mobilecommerce/recogu/MainActivity.java
+++ app/src/main/java/com/csi5175/mobilecommerce/recogu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.csi5175.mobilecommerce.recogu;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -7,10 +8,14 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +30,15 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.w3c.dom.Text;
 
@@ -32,9 +46,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
     public GoogleApiClient mApiClient;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SupportMapFragment mapFragment;
     private String mainName = MainActivity.class.getSimpleName();
     private TextView txtActivity, txtConfidence;
     private ImageView imgActivity;
@@ -52,6 +68,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         txtActivity = findViewById(R.id.txt_activity);
         txtConfidence = findViewById(R.id.txt_confidence);
         imgActivity = (ImageView) findViewById(R.id.img_activity);
@@ -60,10 +81,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         sqlDB = openOrCreateDatabase(DATABASE_NAME, SQLiteDatabase.CREATE_IF_NECESSARY, null);
 //        cursor = sqlDB.query(TABLE_NAME, null, null, null, null, null, null);
-        try{
+        try {
             String createTableQuery = "CREATE TABLE " + TABLE_NAME + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + TYPE + " TEXT NOT NULL, " + TIMESTAMP + " TEXT NOT NULL)";
             sqlDB.execSQL(createTableQuery);
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -88,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 imgActivity.setImageResource(icon);
 
                 // Query for last activity
-                String selectQuery = "SELECT * FROM " + TABLE_NAME + " ORDER BY "+ TIMESTAMP + " DESC LIMIT 1";
+                String selectQuery = "SELECT * FROM " + TABLE_NAME + " ORDER BY " + TIMESTAMP + " DESC LIMIT 1";
                 Cursor cursor = sqlDB.rawQuery(selectQuery, null);
                 if (cursor.moveToFirst()) {
                     String lastTimestamp = cursor.getString(cursor.getColumnIndex(TIMESTAMP));
@@ -97,11 +118,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 //                    int duration = (Integer.valueOf(lastTimestamp)-Integer.valueOf(timestamp))/1000;
                     long duration = 0;
                     try {
-                        duration = (simpleDateFormat.parse(timestamp).getTime() - simpleDateFormat.parse(lastTimestamp).getTime())/1000;
+                        duration = (simpleDateFormat.parse(timestamp).getTime() - simpleDateFormat.parse(lastTimestamp).getTime()) / 1000;
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
-                    if(duration != 0 && !lastActivity.equals("")) {
+                    if (duration != 0 && !lastActivity.equals("")) {
                         String toastText = "You have been " + lastActivity + " for " + Long.toString(duration) + " seconds";
                         Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG).show();
 //                        Toast.makeText(getApplicationContext(), timestamp, Toast.LENGTH_LONG).show();
@@ -158,7 +179,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onStop() {
         super.onStop();
         stopService(new Intent(this, ActivityRecognizedService.class));
-        sqlDB.delete(TABLE_NAME, null,null);
+        sqlDB.delete(TABLE_NAME, null, null);
     }
 
     private void displayGreeting() {
@@ -169,5 +190,49 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         imgActivity.setImageResource(R.drawable.ic_frontpage);
         txtActivity.setGravity(Gravity.CENTER);
         txtActivity.setText("Welcome back! \nCurrent time is \n" + time);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+//        Location location = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.e("permission","no permission");
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.e("location","has location");
+                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            googleMap.addMarker(new MarkerOptions().position(currentLocation).title("You are here"));
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+//                            googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+                            googleMap.getUiSettings().setZoomControlsEnabled(true);
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode != 1) {
+            return;
+        }
+
+        if(Manifest.permission.ACCESS_FINE_LOCATION.equals(permissions[0]) && (grantResults[0]==PackageManager.PERMISSION_GRANTED)) {
+            mapFragment.getMapAsync(this);
+        }
     }
 }
---
+++ app/src/main/java/com/csi5175/mobilecommerce/recogu/MapService.java
@@ -0,0 +1,8 @@
+package com.csi5175.mobilecommerce.recogu;
+
+/**
+ * Created by downson on 2/21/18.
+ */
+
+public class MapService {
+}
